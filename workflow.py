f3e6406c4c1c28449afa27f42956ca518e1469f7
Mathieu Lecarme <mlecarme@bearstech.com>
2014-07-08 19:12:02 +0200

    One ring to rule them all.

--- workflow.py
+++ workflow.py
@@ -32,6 +32,14 @@ class Geonames(luigi.Task):
         with zipfile.ZipFile(self.requires().output().open('r'), 'r') as z:
             z.extractall('data/')
 
+class RunAll(luigi.Task):
+
+    def requires(self):
+        for i in ["FR", "US", "GB", "BE", "RU", "CA", "JP", "PT"]:
+            yield Geonames(i)
+
+    def complete(self):
+        return False
 
 if __name__ == '__main__':
-    luigi.run()
+    luigi.run(main_task_cls=RunAll)
