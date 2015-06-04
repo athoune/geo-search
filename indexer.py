12223e4d048294825ff4ad4dbe51b80b13e121ca
Mathieu Lecarme <mlecarme@bearstech.com>
2015-06-04 18:08:47 +0200

    Choose your nodes.

--- indexer.py
+++ indexer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import sys
 import logging
 
 from elasticsearch import Elasticsearch
@@ -7,8 +6,6 @@ from elasticsearch.helpers import bulk
 
 from reader import read
 from hierarchy import reverse_ancestor
-
-cpt = 0
 
 
 settings = {
@@ -34,9 +31,9 @@ mappings = {
             },
             'suggest': {
                 'type': 'completion',
-                'index_analyzer' : 'simple',
-                'search_analyzer' : 'simple',
-                'payloads' : True
+                'index_analyzer': 'simple',
+                'search_analyzer': 'simple',
+                'payloads': True
             },
             'hierarchy': {
                 'type': 'string',
@@ -54,6 +51,8 @@ mappings = {
     }
 }
 
+cpt = 0
+
 
 def indexer(datas):
     global cpt
@@ -61,43 +60,61 @@ def indexer(datas):
         cpt += 1
         if cpt % 10000 == 0:
             print ".",
-        yield {'name': data['name'],
-               'alternate_name': data['alternatenames'].split(','),
-               '_index': 'geoname',
-               '_type': 'geoname',
-               '_id': data['geonameid'],
-               'cc': data['country_code'],
-               'hierarchy': '/'.join(reverse_ancestor(data['geonameid'])),
-               'location': [data['longitude'], data['latitude']]
-               }
+        yield {
+            'name': data['name'],
+            'suggest': {
+                'input': [data['name']],
+            },
+            'alternate_name': data['alternatenames'].split(','),
+            '_index': 'geoname',
+            '_type': 'geoname',
+            '_id': data['geonameid'],
+            'cc': data['country_code'],
+            'hierarchy': '/'.join(reverse_ancestor(data['geonameid'])),
+            'location': [data['longitude'], data['latitude']]
+        }
 
-logger = logging.getLogger('elasticsearch')
-logger.addHandler(logging.StreamHandler())
-#logger.level = logging.DEBUG
+if __name__ == '__main__':
+    import sys
 
-es = Elasticsearch()
-if es.indices.exists('geoname'):
-    es.indices.delete('geoname')
+    logger = logging.getLogger('elasticsearch')
+    logger.addHandler(logging.StreamHandler())
+    #logger.level = logging.DEBUG
 
-print es.indices.create(index='geoname', body={
-    'mappings': mappings,
-    'settings': settings}
-)
+    if len(sys.argv) == 1:
+        src = 'FR.txt'
+    else:
+        src = sys.argv[1]
+    if len(sys.argv) > 2:
+        nodes = sys.argv[2:]
+    else:
+        nodes = '127.0.0.1'
 
-bulk(es, indexer(read(sys.argv[1])))
+    print("nodes", nodes)
+    es = Elasticsearch(nodes)
 
-es.indices.refresh('geoname')
-# 36m31.860s for 8600414 documents
+    if es.indices.exists('geoname'):
+        es.indices.delete('geoname')
 
-print es.indices.stats('geoname')
-#res = es.search(index="geoname", body={"query": {"match_all": {}}})
-#es.search('geoname', {
-    #'query': {
-        #'filtered': {
-            #'filter': {
-                #'term': {'cc': 'FR'}
-            #},
-            #'query': {
-                #'query_string': {'query': 'Gre%'}
-            #}
-        #}}})
+    print es.indices.create(index='geoname', body={
+        'mappings': mappings,
+        'settings': settings}
+    )
+
+    bulk(es, indexer(read(src)))
+
+    es.indices.refresh('geoname')
+    # 36m31.860s for 8600414 documents
+
+    print es.indices.stats('geoname')
+    #res = es.search(index="geoname", body={"query": {"match_all": {}}})
+    #es.search('geoname', {
+        #'query': {
+            #'filtered': {
+                #'filter': {
+                    #'term': {'cc': 'FR'}
+                #},
+                #'query': {
+                    #'query_string': {'query': 'Gre%'}
+                #}
+            #}}})
